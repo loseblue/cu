b9144acbdc26857208f82e92c56989ecb1585ad7
loseblue <loseblue@gmaiil.com>
2023-06-28 18:32:29 +0800

    Add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 
 use tracing::{info};
-
 mod log;
+use tokio;
 
 
 
 fn main() {
     println!("CU start !");
+    info!("CU start !");
 
     let _guard = log::init();
         
-    info!("CU start !");
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(8)
+        .enable_io()
+        .enable_time()
+        .build()
+        .unwrap();
+
+        println!("CU end !");
 }
 
