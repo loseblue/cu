91fade391d80b86b9922b09e7df685977fe7835a
loseblue <loseblue@gmaiil.com>
2023-07-13 16:22:34 +0800

    Add f1ap interface

---
+++ f1ap/src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn f1ap_server_process(data: Vec<u8>) {
+    println!("f1ap_server_process data: {:?}", data);
+}
--- sctp/src/lib.rs
+++ sctp/src/lib.rs
@@ -1,4 +1,4 @@
 
 pub fn f1ap_server_process(data: Vec<u8>) {
-    println!("f1ap_server_process data: {:?}", data);
+    println!("f1ap_server_process data: {:02x?}", data);
 }
